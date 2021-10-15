488084f232e8a7b2b24aad44ce4c37f3bac6b590
Hassan Khalid Butt <55914809+hbutt877@users.noreply.github.com>
2021-10-15 16:17:07 +0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y python3-pip
+pip3 install selenium==3.141.0
+pip3 install pyvirtualdisplay
+sudo apt install -y unzip
+sudo apt install -y xvfb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+wget -O t.py https://raw.githubusercontent.com/hbutt877/nust/master/t.py
+wget -O job_t.sh 
