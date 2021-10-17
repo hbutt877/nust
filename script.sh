50f4f940c1449a66ff5c992ec3d5b52bda01d019
Hassan Khalid Butt <55914809+hbutt877@users.noreply.github.com>
2021-10-17 16:28:58 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,12 +4,15 @@ pip3 install selenium==3.141.0
 pip3 install pyvirtualdisplay
 sudo apt install -y unzip
 sudo apt install -y xvfb
+sudo apt install -y sqlite3
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 wget -O t.py https://raw.githubusercontent.com/hbutt877/nust/master/t.py
 wget -O job_t.sh https://raw.githubusercontent.com/hbutt877/nust/master/job_t.sh
+wget -O login.py https://raw.githubusercontent.com/hbutt877/nust/master/login.py
+wget -O job_l.sh https://raw.githubusercontent.com/hbutt877/nust/master/job_l.sh
 rm chromedriver_linux64.zip
 rm google-chrome-stable_current_amd64.deb
 mkdir pics
