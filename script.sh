3ed4a24ae8a57412217480ef3fe8b0bec82bf27e
Hassan Khalid Butt <55914809+hbutt877@users.noreply.github.com>
2021-10-17 17:51:55 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt install -y python3-pip
 pip3 install selenium==3.141.0
 pip3 install pyvirtualdisplay
+pip3 install 2captcha-python
 sudo apt install -y unzip
 sudo apt install -y xvfb
 sudo apt install -y sqlite3
@@ -13,6 +14,7 @@ wget -O t.py https://raw.githubusercontent.com/hbutt877/nust/master/t.py
 wget -O job_t.sh https://raw.githubusercontent.com/hbutt877/nust/master/job_t.sh
 wget -O login.py https://raw.githubusercontent.com/hbutt877/nust/master/login.py
 wget -O job_l.sh https://raw.githubusercontent.com/hbutt877/nust/master/job_l.sh
+wget -O captcha.py https://raw.githubusercontent.com/hbutt877/nust/master/captcha.py
 rm chromedriver_linux64.zip
 rm google-chrome-stable_current_amd64.deb
 mkdir pics
